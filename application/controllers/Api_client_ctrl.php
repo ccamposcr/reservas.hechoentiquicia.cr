47ac6539f366b388597a558eb1833a0074c5ff3d
ccamposcr <ccamposcr@gmail.com>
2019-10-12 11:09:03 -0600

    API Client Controller and Model completed

--- application/controllers/Api_client_ctrl.php
+++ application/controllers/Api_client_ctrl.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api_client_ctrl extends CI_Controller {
 
+    private $name;
+    private $lastname;
+    private $email;
+    private $phone;
+    private $address;
+    private $clientID;
+
     public function __construct()
     {
         parent::__construct();
@@ -11,20 +18,30 @@ class Api_client_ctrl extends CI_Controller {
 
     public function setClient(){
 
-        $id = ( isset($_POST['id']) ) ? strip_tags($_POST['id']) : '0';
         $name = ( isset($_POST['name']) ) ? strip_tags($_POST['name']) : '';
         $lastname = ( isset($_POST['lastname']) ) ? strip_tags($_POST['lastname']) : '';
         $email = ( isset($_POST['email']) ) ? strip_tags($_POST['email']) : '';
         $phone = ( isset($_POST['phone']) ) ? strip_tags($_POST['phone']) : '';
         $address = ( isset($_POST['address']) ) ? strip_tags($_POST['address']) : '';
+        $clientID = ( isset($_POST['clientID']) ) ? strip_tags($_POST['clientID']) : '';
 
-        $this->Api_client_model->setClient($id, $name, $lastname, $email, $phone, $address);
+        $this->Api_client_model->setClient($name, $lastname, $email, $phone, $address, $clientID);
     }
 
     public function getClientByID(){
 
         $id = ( isset($_POST['id']) ) ? strip_tags($_POST['id']) : 0;
         $client = $this->Api_client_model->getClientByID($id);
+
+        echo json_encode($client);
+
+    }
+
+    public function getClientByClientID(){
+
+        $clientID = ( isset($_POST['clientID']) ) ? strip_tags($_POST['clientID']) : 0;
+        $client = $this->Api_client_model->getClientByClientID($clientID);
+
         echo json_encode($client);
 
     }
@@ -32,6 +49,7 @@ class Api_client_ctrl extends CI_Controller {
     public function getAllClients(){
 
         $clients = $this->Api_client_model->getAllClients();
+
         echo json_encode($clients);
 
     }
--- application/models/Api_client_model.php
+++ application/models/Api_client_model.php
@@ -1,27 +1,55 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Api_client_model extends CI_MODEL
-{
+class Api_client_model extends CI_MODEL{
     
-    function setClient($id, $name, $lastname, $email, $phone, $address){ 
-        $this->db->query("INSERT INTO client(id, name, lastname, email, phone, address)". 
-        "VALUES (".$this->db->escape($id).
-        ",".$this->db->escape($name).
-        ",".$this->db->escape($lastname).
-        ",".$this->db->escape($email).
-        ",".$this->db->escape($phone).
-        ",".$this->db->escape($address).")");
+    function setClient($name, $lastname, $email, $phone, $address, $clientID){
+
+        $query = $this->db->query("SELECT id FROM client WHERE clientID = " . $this->db->escape($clientID));
+        if ($query->num_rows() > 0){
+            //Update
+            $this->db->query("UPDATE client SET name = ". $this->db->escape($name).
+            ",lastname = " . $this->db->escape($lastname).
+            ",email = " . $this->db->escape($email).
+            ",phone = " . $this->db->escape($phone).
+            ",address = " . $this->db->escape($address).
+            " WHERE clientID = " . $this->db->escape($clientID));
+        }
+        else{
+            //Insert
+            $this->db->query("INSERT INTO client(name, lastname, email, phone, address, clientID)". 
+            "VALUES (".$this->db->escape($name).
+            ",".$this->db->escape($lastname).
+            ",".$this->db->escape($email).
+            ",".$this->db->escape($phone).
+            ",".$this->db->escape($address).
+            ",".$this->db->escape($clientID).")");
+        }
+
     }
 
     function getClientByID($id){
-    	$query = $this->db->query("SELECT * FROM client WHERE id = ".$this->db->escape($id));
+
+        $query = $this->db->query("SELECT * FROM client WHERE id = " . $this->db->escape($id));
+        
         return $query->result_array();
+
+    }
+
+    function getClientByClientID($clientID){
+
+        $query = $this->db->query("SELECT * FROM client WHERE clientID = " . $this->db->escape($clientID));
+        
+        return $query->result_array();
+
     }
 
     function getAllClients(){
-    	$query = $this->db->query("SELECT * FROM client");
+
+        $query = $this->db->query("SELECT * FROM client");
+        
         return $query->result_array();
+
     }
 }
 
